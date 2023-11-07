16c9fa81a71b088aa2d3f7e576ae3c01b2278fe9
Aswin lal <laswin770@gmail.com>
2023-11-07 09:59:11 +0530

    Api completed

---
+++ netflix_clone/lib/core/colors/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const backgroundcolor = Colors.black;
+
+const kwhite = Colors.white;
+final buttoncolorblue = Colors.blueAccent[700];
+const buttoncolorwhite = Colors.white;
+const kblack = Colors.black;
